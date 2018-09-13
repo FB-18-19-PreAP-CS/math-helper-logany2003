e34577ee7223c1f78f9c2acba79267382cc125f4
Logan Young <logany2013@gmail.com>
2018-09-13 14:12:09 -0500

    worked on quadratic

--- math_helper_youngL.py
+++ math_helper_youngL.py
@@ -1,5 +1,4 @@
 from math import *
-
 '''
 formula examples go here
 
@@ -12,3 +11,9 @@ returns the awnser when entered a,b, and c from formula ax^2 + bx + c
     '''
     if a==0:
         return "You have divided by zero, error"
+    x = (b**2) - (4*a*c)
+    y = math.sqrt(x)
+    print(y)
+
+def pyhtagorean_theorm():
+    pass
