6467acebc6b24d1104749232ee973e0505146fcf
Logan Young <logany2013@gmail.com>
2018-09-17 14:12:06 -0500

    made quadratic

--- math_helper_youngL.py
+++ math_helper_youngL.py
@@ -1,19 +1,33 @@
-from math import *
+from math import sqrt
 '''
 formula examples go here
 
 '''
 
 
-def quadratic_formula(a,b,c):
+def quadratic(a,b,c):
     '''
 returns the awnser when entered a,b, and c from formula ax^2 + bx + c
+    >>> quadratic(1,2,3)
+    'No real solutions'
+    >>> quadratic(5,3,3)
+    'No real solutions'
+    >>> quadratic(5,6,1)
+    (-5.0, -5.0)
     '''
+    disc = (b**2) - (4*a*c)
     if a==0:
         return "You have divided by zero, error"
-    x = (b**2) - (4*a*c)
-    y = math.sqrt(x)
-    print(y)
-
+    if disc < 0:
+        return "No real solutions"
+    if disc > 0:
+        awnser1 = (-b + sqrt(disc)) / (2*a)
+        awnser2 = (-b - sqrt(disc)) / (2*a)
+        return awnser1, awnser2
+    if disc == 0:
+        awnser = (-b) / (2*a)
+        return awnser
+    
 def pyhtagorean_theorm():
     pass
+quadratic(5,6,1)
