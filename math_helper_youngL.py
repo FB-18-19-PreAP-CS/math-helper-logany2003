1b6bea38bf4d06787a6b2bb3cb3b21821e39ffcf
Logan Young <logany2013@gmail.com>
2018-09-19 14:10:41 -0500

    fixed horizontal asymptotes, added compound_interest, added distance

--- math_helper_youngL.py
+++ math_helper_youngL.py
@@ -7,7 +7,7 @@ formula examples go here
 
 def quadratic(a,b,c):
     '''
-returns the awnser when entered a,b, and c from formula ax^2 + bx + c
+returns the answer when entered a,b, and c from formula ax^2 + bx + c
     >>> quadratic(1,2,3)
     'No real solutions'
     >>> quadratic(5,3,3)
@@ -25,12 +25,12 @@ returns the awnser when entered a,b, and c from formula ax^2 + bx + c
     if disc < 0:
         return "No real solutions"
     if disc > 0:
-        awnser1 = (-b + sqrt(disc)) / (2*a)
-        awnser2 = (-b - sqrt(disc)) / (2*a)
-        return awnser1, awnser2
+        answer1 = (-b + sqrt(disc)) / (2*a)
+        answer2 = (-b - sqrt(disc)) / (2*a)
+        return answer1, answer2
     if disc == 0:
-        awnser = (-b) / (2*a)
-        return awnser
+        answer = (-b) / (2*a)
+        return answer
     
 def pythagorean(a,b):
     '''
@@ -52,14 +52,29 @@ hypotanuse on a right triangle
     if b <= 0:
         return "You can not have a negative or zero length"
     pythag = a**2 + b**2
-    awnser = sqrt(pythag)
-    return awnser
+    answer = sqrt(pythag)
+    return answer
 
-def horizontal_asymptotes(a,b):
-    
-    if determine powers(a,b) == "divide":
-        divide_coef(c,d)
-    
+def horizontal_asymptotes(a,b,c,d):
+    '''
+enter the powers for a and b and enter the coefficients for c and d. It gives you
+the location of the horizontal asymptote
+    >>> horizontal_asymptotes(3,2,2,3)
+    'No horizontal asymptote'
+    >>> horizontal_asymptotes(1,5,1,1)
+    (0)
+    >>> horizontal_asymptotes(1,1,2,1)
+    (2.0)
+    >>> horizontal_asymptotes(0,1,-1,1)
+    (0)
+    >>> horizontal_asymptotes(17,1,1,0)
+    'No horizontal asymptote'
+    '''
+    if determine_powers(a,b) == "divide":
+        return divide_coef(c,d)
+    else:
+        return determine_powers(a,b)
+        
 def determine_powers(a,b):
     '''
 input the powers of the top and bottom of the fraction and determines where the
@@ -70,7 +85,7 @@ horizontal asymptote will be
     if a == b:
         return "divide"
     if a > b:
-        return "None"
+        return "No horizontal asymptote"
 
 def divide_coef(a,b):
     '''
@@ -80,5 +95,18 @@ horizontal asymptote
     location = a / b
     return location
 
-def interest:
-    pass
+def compound_interest(principal, rate, compound_amount, time):
+    '''
+takes in the initial amount, interest rate(as a decimal), amount of times interest is compounded
+per year, and the time in years
+    >>> compound_interest(0,0,0,0)
+    '''
+    if compound_amount == 0:
+        return "you cannot compound interest 0 times a year"
+    amount = principal*(1 + (rate/compound_amount))**(compound_amount*time)
+    amount = round(amount,2)
+    return amount
+    
+def distance(x1,y1,x2,y2):  
+    dist = sqrt((x1-x2)**2 + (y1-y2)**2 )
+    return dist
